0e22e0399ffbd396f86b13782b74ef305bcc0b24
BuildTools <unconfigured@null.spigotmc.org>
2024-12-23 16:50:55 +0900

    1.20.4完成

--- src/main/java/com/github/tacowasa059/removedirectconnectionbutton/mixin/JoinMultiplayerScreenMixin.java
+++ src/main/java/com/github/tacowasa059/removedirectconnectionbutton/mixin/JoinMultiplayerScreenMixin.java
@@ -2,10 +2,7 @@ package com.github.tacowasa059.removedirectconnectionbutton.mixin;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.gui.components.Button;
-import net.minecraft.client.gui.layouts.FrameLayout;
-import net.minecraft.client.gui.layouts.GridLayout;
-import net.minecraft.client.gui.layouts.LinearLayout;
-import net.minecraft.client.gui.layouts.SpacerElement;
+import net.minecraft.client.gui.layouts.*;
 import net.minecraft.client.gui.screens.ConfirmScreen;
 import net.minecraft.client.gui.screens.EditServerScreen;
 import net.minecraft.client.gui.screens.Screen;
@@ -74,7 +71,7 @@ public abstract class JoinMultiplayerScreenMixin extends Screen{
     private void removeDirectJoinButton(CallbackInfo ci) {
         JoinMultiplayerScreen screen = (JoinMultiplayerScreen) (Object)this;
         if (this.initedOnce) {
-            this.serverSelectionList.updateSize(this.width, this.height, 32, this.height - 64);
+            this.serverSelectionList.setRectangle(this.width, this.height - 64 - 32, 0, 32);
         } else {
             this.initedOnce = true;
             this.servers = new ServerList(this.minecraft);
@@ -85,10 +82,10 @@ public abstract class JoinMultiplayerScreenMixin extends Screen{
                 this.lanServerDetector = new LanServerDetection.LanServerDetector(this.lanServerList);
                 this.lanServerDetector.start();
             } catch (Exception exception) {
-                LOGGER.warn("Unable to start LAN server detection: {}", (Object)exception.getMessage());
+                LOGGER.warn("Unable to start LAN server detection: {}", exception.getMessage());
             }
 
-            this.serverSelectionList = new ServerSelectionList(screen, this.minecraft, this.width, this.height, 32, this.height - 64, 36);
+            this.serverSelectionList = new ServerSelectionList(screen, this.minecraft, this.width, this.height - 64 - 32, 32, 36);
             this.serverSelectionList.updateOnlineServers(this.servers);
         }
 
@@ -96,15 +93,16 @@ public abstract class JoinMultiplayerScreenMixin extends Screen{
         this.selectButton = this.addRenderableWidget(Button.builder(Component.translatable("selectServer.select"), (p_99728_) -> {
             this.joinSelectedServer();
         }).width(152).build());
-        Button button1 = this.addRenderableWidget(Button.builder(Component.translatable("selectServer.add"), (p_280869_) -> {
-            this.editingServer = new ServerData(I18n.get("selectServer.defaultName"), "", false);
+
+        Button button1 = this.addRenderableWidget(Button.builder(Component.translatable("selectServer.add"), (p_296190_) -> {
+            this.editingServer = new ServerData(I18n.get("selectServer.defaultName"), "", ServerData.Type.OTHER);
             this.minecraft.setScreen(new EditServerScreen(this, this::addServerCallback, this.editingServer));
         }).width(152).build());
         this.editButton = this.addRenderableWidget(Button.builder(Component.translatable("selectServer.edit"), (p_99715_) -> {
             ServerSelectionList.Entry serverselectionlist$entry = this.serverSelectionList.getSelected();
             if (serverselectionlist$entry instanceof ServerSelectionList.OnlineServerEntry) {
                 ServerData serverdata = ((ServerSelectionList.OnlineServerEntry)serverselectionlist$entry).getServerData();
-                this.editingServer = new ServerData(serverdata.name, serverdata.ip, false);
+                this.editingServer = new ServerData(serverdata.name, serverdata.ip, ServerData.Type.OTHER);
                 this.editingServer.copyFrom(serverdata);
                 this.minecraft.setScreen(new EditServerScreen(this, this::editServerCallback, this.editingServer));
             }
@@ -127,22 +125,22 @@ public abstract class JoinMultiplayerScreenMixin extends Screen{
         Button button2 = this.addRenderableWidget(Button.builder(Component.translatable("selectServer.refresh"), (p_99706_) -> {
             this.refreshServerList();
         }).width(74).build());
-        Button button3 = this.addRenderableWidget(Button.builder(CommonComponents.GUI_CANCEL, (p_280867_) -> {
+        Button button3 = this.addRenderableWidget(Button.builder(CommonComponents.GUI_BACK, (p_280867_) -> {
             this.minecraft.setScreen(this.lastScreen);
         }).width(74).build());
-        GridLayout gridlayout = new GridLayout();
-        GridLayout.RowHelper gridlayout$rowhelper = gridlayout.createRowHelper(1);
-        LinearLayout linearlayout = gridlayout$rowhelper.addChild(new LinearLayout(308, 20, LinearLayout.Orientation.HORIZONTAL));
-        linearlayout.addChild(this.selectButton);
-        linearlayout.addChild(button1);
-        gridlayout$rowhelper.addChild(SpacerElement.height(4));
-        LinearLayout linearlayout1 = gridlayout$rowhelper.addChild(new LinearLayout(308, 20, LinearLayout.Orientation.HORIZONTAL));
-        linearlayout1.addChild(this.editButton);
-        linearlayout1.addChild(this.deleteButton);
-        linearlayout1.addChild(button2);
-        linearlayout1.addChild(button3);
-        gridlayout.arrangeElements();
-        FrameLayout.centerInRectangle(gridlayout, 0, this.height - 64, this.width, 64);
+        LinearLayout linearlayout = LinearLayout.vertical();
+        EqualSpacingLayout equalspacinglayout = linearlayout.addChild(new EqualSpacingLayout(308, 20, EqualSpacingLayout.Orientation.HORIZONTAL));
+        equalspacinglayout.addChild(this.selectButton);
+
+        equalspacinglayout.addChild(button1);
+        linearlayout.addChild(SpacerElement.height(4));
+        EqualSpacingLayout equalspacinglayout1 = linearlayout.addChild(new EqualSpacingLayout(308, 20, EqualSpacingLayout.Orientation.HORIZONTAL));
+        equalspacinglayout1.addChild(this.editButton);
+        equalspacinglayout1.addChild(this.deleteButton);
+        equalspacinglayout1.addChild(button2);
+        equalspacinglayout1.addChild(button3);
+        linearlayout.arrangeElements();
+        FrameLayout.centerInRectangle(linearlayout, 0, this.height - 64, this.width, 64);
         this.onSelectedChange();
 
         ci.cancel();
