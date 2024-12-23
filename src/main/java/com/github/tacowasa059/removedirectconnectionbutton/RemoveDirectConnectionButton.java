8906ebbedea479e669870c254ae7da6bfdd42c7e
BuildTools <unconfigured@null.spigotmc.org>
2024-12-23 09:47:33 +0900

    1.16.5完成

--- src/main/java/com/github/tacowasa059/removedirectconnectionbutton/RemoveDirectConnectionButton.java
+++ src/main/java/com/github/tacowasa059/removedirectconnectionbutton/RemoveDirectConnectionButton.java
@@ -3,10 +3,7 @@ package com.github.tacowasa059.removedirectconnectionbutton;
 import net.minecraftforge.fml.common.Mod;
 @Mod(RemoveDirectConnectionButton.MODID)
 public class RemoveDirectConnectionButton {
-
-    // Define mod id in a common place for everything to reference
     public static final String MODID = "removedirectconnectionbutton";
     public RemoveDirectConnectionButton() {
     }
-
 }
--- src/main/java/com/github/tacowasa059/removedirectconnectionbutton/mixin/JoinMultiplayerScreenMixin.java
+++ src/main/java/com/github/tacowasa059/removedirectconnectionbutton/mixin/JoinMultiplayerScreenMixin.java
@@ -1,35 +1,27 @@
 package com.github.tacowasa059.removedirectconnectionbutton.mixin;
 
-import com.mojang.logging.LogUtils;
-import net.minecraft.client.gui.components.Button;
-import net.minecraft.client.gui.layouts.FrameLayout;
-import net.minecraft.client.gui.layouts.GridLayout;
-import net.minecraft.client.gui.layouts.LinearLayout;
-import net.minecraft.client.gui.layouts.SpacerElement;
-import net.minecraft.client.gui.screens.ConfirmScreen;
-import net.minecraft.client.gui.screens.EditServerScreen;
-import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.client.gui.screens.multiplayer.JoinMultiplayerScreen;
-import net.minecraft.client.gui.screens.multiplayer.ServerSelectionList;
+import net.minecraft.client.gui.DialogTexts;
+import net.minecraft.client.gui.screen.*;
+import net.minecraft.client.gui.widget.button.Button;
 import net.minecraft.client.multiplayer.ServerData;
 import net.minecraft.client.multiplayer.ServerList;
-import net.minecraft.client.resources.language.I18n;
-import net.minecraft.client.server.LanServerDetection;
-import net.minecraft.network.chat.CommonComponents;
-import net.minecraft.network.chat.Component;
-import org.slf4j.Logger;
+import net.minecraft.client.network.LanServerDetector;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import javax.annotation.Nullable;
-
-@Mixin(JoinMultiplayerScreen.class)
-public abstract class JoinMultiplayerScreenMixin extends Screen{
-    @Shadow
-    private static final Logger LOGGER = LogUtils.getLogger();
+@Mixin(MultiplayerScreen.class)
+public abstract class JoinMultiplayerScreenMixin extends Screen {
+    private static final Logger LOGGER = LogManager.getLogger();
     @Shadow
     private  Screen lastScreen;
     @Shadow
@@ -45,12 +37,15 @@ public abstract class JoinMultiplayerScreenMixin extends Screen{
     @Shadow
     private ServerData editingServer;
     @Shadow
-    private LanServerDetection.LanServerList lanServerList;
+    private LanServerDetector.LanServerList lanServerList;
     @Shadow
-    @Nullable
-    private LanServerDetection.LanServerDetector lanServerDetector;
+    private LanServerDetector.LanServerFindThread lanServerDetector;
     @Shadow
     private boolean initedOnce;
+
+    protected JoinMultiplayerScreenMixin(ITextComponent p_i51108_1_) {
+        super(p_i51108_1_);
+    }
 
     @Shadow
     protected abstract void joinSelectedServer();
@@ -66,83 +61,69 @@ public abstract class JoinMultiplayerScreenMixin extends Screen{
     @Shadow
     protected abstract void refreshServerList();
 
-    protected JoinMultiplayerScreenMixin(Component p_96550_) {
-        super(p_96550_);
-    }
-
     @Inject(method = "init", at = @At("HEAD"), cancellable = true)
     private void removeDirectJoinButton(CallbackInfo ci) {
-        JoinMultiplayerScreen screen = (JoinMultiplayerScreen) (Object)this;
+        MultiplayerScreen screen = (MultiplayerScreen) (Object)this;
+        super.init();
+        this.minecraft.keyboardHandler.setSendRepeatsToGui(true);
         if (this.initedOnce) {
             this.serverSelectionList.updateSize(this.width, this.height, 32, this.height - 64);
         } else {
             this.initedOnce = true;
             this.servers = new ServerList(this.minecraft);
             this.servers.load();
-            this.lanServerList = new LanServerDetection.LanServerList();
+            this.lanServerList = new LanServerDetector.LanServerList();
 
             try {
-                this.lanServerDetector = new LanServerDetection.LanServerDetector(this.lanServerList);
+                this.lanServerDetector = new LanServerDetector.LanServerFindThread(this.lanServerList);
                 this.lanServerDetector.start();
-            } catch (Exception exception) {
-                LOGGER.warn("Unable to start LAN server detection: {}", (Object)exception.getMessage());
+            } catch (Exception var2) {
+                LOGGER.warn("Unable to start LAN server detection: {}", var2.getMessage());
             }
 
             this.serverSelectionList = new ServerSelectionList(screen, this.minecraft, this.width, this.height, 32, this.height - 64, 36);
             this.serverSelectionList.updateOnlineServers(this.servers);
         }
 
-        this.addWidget(this.serverSelectionList);
-        this.selectButton = this.addRenderableWidget(Button.builder(Component.translatable("selectServer.select"), (p_99728_) -> {
+        this.children.add(this.serverSelectionList);
+        this.selectButton = (Button)this.addButton(new Button(this.width / 2 - 154, this.height - 52, 152, 20, new TranslationTextComponent("selectServer.select"), (p_214293_1_) -> {
             this.joinSelectedServer();
-        }).width(152).build());
-        Button button1 = this.addRenderableWidget(Button.builder(Component.translatable("selectServer.add"), (p_280869_) -> {
+        }));
+
+        this.addButton(new Button(this.width / 2 + 4 + 50, this.height - 52, 152, 20, new TranslationTextComponent("selectServer.add"), (p_214288_1_) -> {
             this.editingServer = new ServerData(I18n.get("selectServer.defaultName"), "", false);
-            this.minecraft.setScreen(new EditServerScreen(this, this::addServerCallback, this.editingServer));
-        }).width(152).build());
-        this.editButton = this.addRenderableWidget(Button.builder(Component.translatable("selectServer.edit"), (p_99715_) -> {
+            this.minecraft.setScreen(new AddServerScreen(this, this::addServerCallback, this.editingServer));
+        }));
+        this.editButton = (Button)this.addButton(new Button(this.width / 2 - 154, this.height - 28, 70, 20, new TranslationTextComponent("selectServer.edit"), (p_214283_1_) -> {
             ServerSelectionList.Entry serverselectionlist$entry = this.serverSelectionList.getSelected();
-            if (serverselectionlist$entry instanceof ServerSelectionList.OnlineServerEntry) {
-                ServerData serverdata = ((ServerSelectionList.OnlineServerEntry)serverselectionlist$entry).getServerData();
+            if (serverselectionlist$entry instanceof ServerSelectionList.NormalEntry) {
+                ServerData serverdata = ((ServerSelectionList.NormalEntry)serverselectionlist$entry).getServerData();
                 this.editingServer = new ServerData(serverdata.name, serverdata.ip, false);
                 this.editingServer.copyFrom(serverdata);
-                this.minecraft.setScreen(new EditServerScreen(this, this::editServerCallback, this.editingServer));
+                this.minecraft.setScreen(new AddServerScreen(this, this::editServerCallback, this.editingServer));
             }
 
-        }).width(74).build());
-        this.deleteButton = this.addRenderableWidget(Button.builder(Component.translatable("selectServer.delete"), (p_99710_) -> {
-            ServerSelectionList.Entry serverselectionlist$entry = this.serverSelectionList.getSelected();
-            if (serverselectionlist$entry instanceof ServerSelectionList.OnlineServerEntry) {
-                String s = ((ServerSelectionList.OnlineServerEntry)serverselectionlist$entry).getServerData().name;
+        }));
+        this.deleteButton = (Button)this.addButton(new Button(this.width / 2 - 74, this.height - 28, 70, 20, new TranslationTextComponent("selectServer.delete"), (p_214294_1_) -> {
+            ServerSelectionList.Entry serverselectionlist$entry = (ServerSelectionList.Entry)this.serverSelectionList.getSelected();
+            if (serverselectionlist$entry instanceof ServerSelectionList.NormalEntry) {
+                String s = ((ServerSelectionList.NormalEntry)serverselectionlist$entry).getServerData().name;
                 if (s != null) {
-                    Component component = Component.translatable("selectServer.deleteQuestion");
-                    Component component1 = Component.translatable("selectServer.deleteWarning", s);
-                    Component component2 = Component.translatable("selectServer.deleteButton");
-                    Component component3 = CommonComponents.GUI_CANCEL;
-                    this.minecraft.setScreen(new ConfirmScreen(this::deleteCallback, component, component1, component2, component3));
+                    ITextComponent itextcomponent = new TranslationTextComponent("selectServer.deleteQuestion");
+                    ITextComponent itextcomponent1 = new TranslationTextComponent("selectServer.deleteWarning", new Object[]{s});
+                    ITextComponent itextcomponent2 = new TranslationTextComponent("selectServer.deleteButton");
+                    ITextComponent itextcomponent3 = DialogTexts.GUI_CANCEL;
+                    this.minecraft.setScreen(new ConfirmScreen(this::deleteCallback, itextcomponent, itextcomponent1, itextcomponent2, itextcomponent3));
                 }
             }
 
-        }).width(74).build());
-        Button button2 = this.addRenderableWidget(Button.builder(Component.translatable("selectServer.refresh"), (p_99706_) -> {
+        }));
+        this.addButton(new Button(this.width / 2 + 4, this.height - 28, 70, 20, new TranslationTextComponent("selectServer.refresh"), (p_214291_1_) -> {
             this.refreshServerList();
-        }).width(74).build());
-        Button button3 = this.addRenderableWidget(Button.builder(CommonComponents.GUI_CANCEL, (p_280867_) -> {
+        }));
+        this.addButton(new Button(this.width / 2 + 4 + 76, this.height - 28, 75, 20, DialogTexts.GUI_CANCEL, (p_214289_1_) -> {
             this.minecraft.setScreen(this.lastScreen);
-        }).width(74).build());
-        GridLayout gridlayout = new GridLayout();
-        GridLayout.RowHelper gridlayout$rowhelper = gridlayout.createRowHelper(1);
-        LinearLayout linearlayout = gridlayout$rowhelper.addChild(new LinearLayout(308, 20, LinearLayout.Orientation.HORIZONTAL));
-        linearlayout.addChild(this.selectButton);
-        linearlayout.addChild(button1);
-        gridlayout$rowhelper.addChild(SpacerElement.height(4));
-        LinearLayout linearlayout1 = gridlayout$rowhelper.addChild(new LinearLayout(308, 20, LinearLayout.Orientation.HORIZONTAL));
-        linearlayout1.addChild(this.editButton);
-        linearlayout1.addChild(this.deleteButton);
-        linearlayout1.addChild(button2);
-        linearlayout1.addChild(button3);
-        gridlayout.arrangeElements();
-        FrameLayout.centerInRectangle(gridlayout, 0, this.height - 64, this.width, 64);
+        }));
         this.onSelectedChange();
 
         ci.cancel();
